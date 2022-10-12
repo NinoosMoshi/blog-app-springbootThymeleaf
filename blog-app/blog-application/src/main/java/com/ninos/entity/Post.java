028921d79c8cfc5b3955473a09672e4c6bdee6ae
NinoosMoshi <nalkaton@gmail.com>
2022-10-12 09:51:13 -0400

    Post class Closes #2

---
+++ blog-app/blog-application/src/main/java/com/ninos/entity/Post.java
@@ -0,0 +1,38 @@
+package com.ninos.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    private String url;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+}
